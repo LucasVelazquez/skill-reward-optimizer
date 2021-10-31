f83997445b90dd4978b7284fd6b91c3bb6fc2c08
LucasVelazquez <lucasvelazquez867@gmail.com>
2021-10-31 16:54:28 -0300

    Code improvements.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,71 +1,90 @@
-import env from './modules/environment.js';
+import env from "./modules/environment.js";
 
 const web3 = new Web3(env.BSC_MAINET_NETWORK);
+let contract = null;
 
-const resultDiv = document.getElementById('result-div');
-const oracleSkillPriceText = document.getElementById('oracle-price');
-const marketSkillPriceText = document.getElementById('market-price');
-const rewardResultText = document.getElementById('reward-result');
+const resultDiv = document.getElementById("result-div");
+const oracleSkillPriceText = document.getElementById("oracle-price");
+const marketSkillPriceText = document.getElementById("market-price");
+const rewardResultText = document.getElementById("reward-result");
 
-const calculateButton = document.getElementById('calculate-button');
-calculateButton.addEventListener('click', calculateReward)
+const calculateButton = document.getElementById("calculate-button");
+calculateButton.addEventListener("click", calculateReward);
 
-function calculateReward() {
-    resultDiv.style.display = 'none';
-    calculateButton.disabled = true;
-    calculateButton.textContent = 'Loading...';
-    fetch(env.BSCSCAN_API_URL)
-        .then(response => response.json())
-        .then((data) => {
-            let contractABI = data.result;
-            if (contractABI === '') throw 'Contract ABI not found.';
+function getContractAbi() {
+  fetch(env.BSCSCAN_API_URL)
+    .then((response) => response.json())
+    .then((contractAbi) => {
+      if (contractAbi.result === "") {
+        throw "Contract ABI not found";
+      }
 
-            let contract = new web3.eth.Contract(JSON.parse(contractABI), env.CONTRACT_ADDRESS);
-            contract.methods.currentPrice()
-                .call((err, res) => {
-                    if (err) throw err;
-
-                    const currentPriceWei = res;
-                    let bnbPriceConvertion = currentPriceWei / env.WEI_UNIT;
-                    let oracleSkillPrice = parseFloat((1 / bnbPriceConvertion).toFixed(2));
-
-                    fetch(env.COINGECKO_API_URL + '/simple/token_price/binance-smart-chain?contract_addresses=' + env.CRYPTO_BLADES_TOKEN_CONTRACT + '&vs_currencies=usd')
-                        .then(response => response.json())
-                        .then((response) => {
-                            oracleSkillPriceText.innerHTML = oracleSkillPrice;
-                            let marketSkillPrice = response[env.CRYPTO_BLADES_TOKEN_CONTRACT].usd;
-                            marketSkillPriceText.innerHTML = marketSkillPrice;
-
-                            let resultText = 'The skill reward is ';
-                            if (oracleSkillPrice > marketSkillPrice) {
-                                rewardResultText.innerHTML = resultText + 'LOW.';
-                                rewardResultText.style.color = '#ec4b4b';
-                            }
-                            else if (oracleSkillPrice < marketSkillPrice) {
-                                rewardResultText.innerHTML = resultText + 'HIGH.';
-                                rewardResultText.style.color = '#28a745';
-                            }
-                            else {
-                                rewardResultText.innerHTML = resultText + 'the SAME.'
-                                rewardResultText.style.color = '#9e8a57'
-                            }
-
-                            resultDiv.style.display = 'block';
-
-                        })
-                        .catch((err) => {
-                            throw err
-                        })
-                        .finally(() => {
-                            calculateButton.textContent = 'Calculate again';
-                            calculateButton.disabled = false;
-                        })
-                });
-        })
-        .catch((error) => {
-            console.log('Error: ' + error);
-        })
+      contract = new web3.eth.Contract(JSON.parse(contractAbi.result), env.CONTRACT_ADDRESS);
+      calculateReward();
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    });
 }
 
-window.addEventListener('load', calculateReward);
+function calculateReward() {
+  hideResults();
+  contract.methods.currentPrice().call((err, currentPriceWei) => {
+    if (err) throw err;
+    let oracleSkillPrice = calculateOracleSkillPrice(currentPriceWei);
+    fetch(
+      `${env.COINGECKO_API_URL}/simple/token_price/binance-smart-chain?contract_addresses=${env.CRYPTO_BLADES_TOKEN_CONTRACT}&vs_currencies=usd`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        let marketSkillPrice = response[env.CRYPTO_BLADES_TOKEN_CONTRACT].usd;
+        setPricesInView(oracleSkillPrice, marketSkillPrice);
+      })
+      .catch((err) => {
+        throw err;
+      })
+      .finally(() => {
+        showResults();
+      });
+  });
+}
 
+function hideResults() {
+  calculateButton.disabled = true;
+  calculateButton.textContent = "Loading...";
+  resultDiv.style.display = "none";
+}
+
+function showResults() {
+  calculateButton.textContent = "Calculate again";
+  calculateButton.disabled = false;
+  resultDiv.style.display = "block";
+}
+
+function setPricesInView(oracleSkillPrice, marketSkillPrice) {
+  oracleSkillPriceText.innerHTML = oracleSkillPrice;
+  marketSkillPriceText.innerHTML = marketSkillPrice;
+
+  let priceText = {
+    text: "the SAME",
+    color: "#9e8a57",
+  };
+
+  if (oracleSkillPrice > marketSkillPrice) {
+    priceText.text = "LOW";
+    priceText.color = "#ec4b4b";
+  } else if (oracleSkillPrice < marketSkillPrice) {
+    priceText.text = "HIGH";
+    priceText.color = "#28a745";
+  }
+
+  rewardResultText.innerHTML = `The skill reward is ${priceText.text}.`;
+  rewardResultText.style.color = priceText.color;
+}
+
+function calculateOracleSkillPrice(priceWei) {
+  const bnbPriceConvertion = priceWei / env.WEI_UNIT;
+  return parseFloat((1 / bnbPriceConvertion).toFixed(2));
+}
+
+window.addEventListener("load", getContractAbi);
